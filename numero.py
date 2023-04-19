eda7d76ade21700712679e4217a9b87a49d549d6
Paulo Santos <paulo_360nd@outlook.com>
2023-04-19 09:23:49 -0300

    Atualizar

---
+++ numero.py
@@ -0,0 +1,14 @@
+print("Trabalhando com números")
+
+numero1 = int(input("Entre com o 1° número: "))
+numero2 = int(input("Entre com o 2° número: "))
+
+soma = numero1 + numero2
+sob = numero1 - numero2
+mult = numero1 * numero2
+divi = numero1 / numero2
+
+print("O valor da soma é: ", soma)
+print("O valor da subtração é: ", sob)
+print("O valor da multiplicação é: ", mult)
+print("O valor da divisão é: ", divi)
