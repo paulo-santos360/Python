c7ca09dd07e185d0f5673c8f93f2d05a9ea79058
Paulo Santos <paulo_360nd@outlook.com>
2023-04-19 08:38:16 -0300

    Atualizar

---
+++ main.py
@@ -0,0 +1,11 @@
+A = 3
+B = 4
+soma = A + B
+menos = A - B
+multiplicacao = A * B
+dividir = A / B
+
+print('a soma ', soma)
+print('a soma ', menos)
+print('a soma ', multiplicacao)
+print('a soma ', dividir)
